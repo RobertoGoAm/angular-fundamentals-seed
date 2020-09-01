6443570f579e5dc3775a02684b4ff44f58f9bad7
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-01 21:59:04 +0200

    Rendering flows - Safe navigation operator

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
+
 interface Passenger {
   id: number,
   fullname: string,
   checkedIn: boolean,
   checkInDate: number | null,
+  children: Child[] | null,
 }
 
 @Component({
@@ -17,10 +23,16 @@ interface Passenger {
         <li *ngFor="let passenger of passengers; let i = index">
           <span class="status" [class.checked-in]="passenger.checkedIn"></span>
           {{ i }}: {{ passenger.fullname }}
-          <p>{{ passenger | json }}</p>
           <div class="date">
             Check in date:
-            {{ passenger.checkInDate ? (passenger.checkInDate  | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
+            {{
+              passenger.checkInDate
+                ? (passenger.checkInDate | date: "yMMMMd" | uppercase)
+                : "Not checked in"
+            }}
+          </div>
+          <div class="children">
+            Children: {{ passenger.children?.length || 0 }}
           </div>
         </li>
       </ul>
@@ -34,30 +46,38 @@ export class AppComponent {
       fullname: "Stephen",
       checkedIn: true,
       checkInDate: 1490742000000,
+      children: null,
     },
     {
       id: 2,
       fullname: "Rose",
       checkedIn: false,
       checkInDate: null,
+      children: [
+        { name: "Ted", age: 12 },
+        { name: "Chloe", age: 7 },
+      ],
     },
     {
       id: 3,
       fullname: "James",
       checkedIn: true,
       checkInDate: 1491606000000,
+      children: null,
     },
     {
       id: 4,
       fullname: "Louise",
       checkedIn: true,
       checkInDate: 1488412800000,
+      children: [{ name: "Jessica", age: 1 }],
     },
     {
       id: 5,
       fullname: "Tina",
       checkedIn: false,
       checkInDate: null,
+      children: null,
     },
   ];
 }
