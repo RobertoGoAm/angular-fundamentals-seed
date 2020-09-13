b25b60393e4256ad8510d181ee21567bd5ef59b9
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-13 17:55:11 +0200

    Component Routing - Wildcard routes for 404 handling

---
+++ app/not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'not-found',
+  template: `
+    <div>
+      Airline passenger app!
+    </div>
+  `
+})
+export class NotFoundComponent {
+
+}
