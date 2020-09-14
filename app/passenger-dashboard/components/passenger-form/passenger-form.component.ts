431fb87a0358ae833c331ac6495aabc3a75f372b
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-14 12:57:14 +0200

    Dynamic Routing - Child and dynamic routes

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -8,7 +8,6 @@ import { Passenger } from "../../models/Passenger.interface";
   styleUrls: ["passenger-form.component.scss"],
   template: `
     <form (ngSubmit)="handleSubmit(form.value, form.valid)" #form="ngForm" novalidate>
-      {{ detail | json }}
       <div>
         Passenger name:
         <input
